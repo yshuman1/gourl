f5bc9c38e2fc2a9c51e827173bf2aa2365f6cf52
yshuman1 <yshuman@loyalloops.com>
2017-09-29 03:57:40 -0700

    1st commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kataras/iris"
+)
+
+// Open a connection to Redis
+var redisStorage = redis.NewClient(&redis.Options{
+	Addr: fmt.Sprintf(
+		"%s:%s",
+		os.Getenv("REDIS_HOST"),
+		os.Getenv("REDIS_PORT"),
+	),
+})
+
+// Custom `*iris.Context` struct to bind our functions to
+type UrlAPI struct {
+	*iris.Context
+}
+
+// Handler for Post route
+func (u UrlAPI) Post() {
+	url := u.FormValue("url")
+	u.Write(url)
+}
+
+func main() {
+	defer redisStorage.Close()
+
+	iris.API("/", UrlAPI{})
+	iris.Listen(fmt.Sprintf("0.0.0.0:%s", os.Getenv("IRIS_PORT")))
+}
